109df3efeaaa04180cbd1e5b6d85bc531dd5057e
Mitchell Hoffmann <mitchellhoffmann1@gmail.com>
2019-02-08 20:46:26 -0600

    Initial Commit

---
+++ Swift Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Basics
+//
+//  Created by Mitchell John Hoffmann on 2/8/19.
+//  Copyright © 2019 Mitchell John Hoffmann. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
